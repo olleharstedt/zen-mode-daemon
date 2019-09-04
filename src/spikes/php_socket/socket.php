0760b3e23a414ec8e54a559b69576a4c77d456a0
Olle Harstedt <olleharstedt@yahoo.com>
2019-09-05 00:03:20 +0200

    Answer with HTTP on PHP socket

--- src/spikes/php_socket/socket.php
+++ src/spikes/php_socket/socket.php
@@ -4,14 +4,16 @@ error_reporting(E_ALL);
 set_time_limit(0);
 ob_implicit_flush();
 
-$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-unlink('/tmp/test.html');
+//unlink('/tmp/test.html');
 
-socket_bind($sock, '/tmp/test.html');
+if (socket_bind($sock, '127.0.0.10', 888) === false) {
+    die('cannot bind' . PHP_EOL);
+}
 
 if (socket_listen($sock, 5) === false) {
-    die('cannot listen');
+    die('cannot listen' . PHP_EOL);
 }
 
 do {
@@ -19,9 +21,11 @@ do {
         echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
     }
 
-    $msg = "\nWelcome to the PHP Test Server. \n" .
-        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+    $msg = "HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n"
+        . "<!DOCTYPE html><html><head></head><body><h1>asd</h1></body></html>\r\n\r\n";
     socket_write($msgsock, $msg, strlen($msg));
+    socket_close($msgsock);
+    continue;
 
     do {
         if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
@@ -38,7 +42,8 @@ do {
             socket_close($msgsock);
             break 2;
         }
-        $talkback = "PHP: You said '$buf'.\n";
+        echo $buf . PHP_EOL;
+        $talkback = "<!DOCTYPE html><html></html>";
         socket_write($msgsock, $talkback, strlen($talkback));
         echo "$buf\n";
     } while (true);
