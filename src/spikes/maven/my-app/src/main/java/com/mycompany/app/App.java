2c0780aae2ee2b626936b5ed9b66a380bb7a422a
Olle Harstedt <olleharstedt@yahoo.com>
2019-09-06 23:12:33 +0200

    Java socket test working

--- src/spikes/maven/my-app/src/main/java/com/mycompany/app/App.java
+++ src/spikes/maven/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
-import java.net.URL;
+import java.net.*;
+import java.io.*;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 
@@ -10,11 +11,28 @@ import org.jsoup.nodes.*;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-        String html = "<html><head><title>First parse</title></head>"
-              + "<body><p>Parsed HTML into a doc.</p></body></html>";
-        Document doc = Jsoup.parse(html);
+        //System.out.println( "Hello World!" );
+        //String html = "<html><head><title>First parse</title></head>"
+              //+ "<body><p>Parsed HTML into a doc.</p></body></html>";
+        //Document doc = Jsoup.parse(html);
+
+        ServerSocket welcomeSocket = new ServerSocket(777);
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            InputStreamReader inputStreamReader = new InputStreamReader(connectionSocket.getInputStream());
+            BufferedReader inFromClient = new BufferedReader(inputStreamReader);
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            String clientSentence = inFromClient.readLine();
+            if (clientSentence != null) {
+                System.out.println("Received: " + clientSentence);
+                outToClient.writeBytes(clientSentence);
+            } else {
+                System.out.println("nothing from the client");
+                System.exit(0);
+            }
+        }
     }
 }
