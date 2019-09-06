66eb38248c6105ba6be3036b75a3d2f2fc629c23
Olle Harstedt <olleharstedt@yahoo.com>
2019-09-06 22:37:14 +0200

    Test dom4j

--- src/spikes/maven/my-app/src/main/java/com/mycompany/app/App.java
+++ src/spikes/maven/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,10 @@
 package com.mycompany.app;
 
+import java.net.URL;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SAXReader reader = new SAXReader();
+        String url = "https://google.com";
+        try {
+            Document document = reader.read(url);
+        } catch (DocumentException ex) {
+            System.out.println("exception: " + ex);
+        }
     }
 }
