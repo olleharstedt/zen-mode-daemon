14e903873e6f85da68817f93e9c3db39b74ce7c4
Olle Harstedt <olleharstedt@yahoo.com>
2019-09-10 22:55:12 +0200

    Testing KMeans clustering for search result

--- src/spikes/php_socket/classes/sites/SearchEngineResultSite.php
+++ src/spikes/php_socket/classes/sites/SearchEngineResultSite.php
@@ -3,6 +3,8 @@
 namespace zenmodedaemon\classes\sites;
 
 use PHPHtmlParser\Dom;
+use Phpml\Classification\KNearestNeighbors;
+use Phpml\Clustering\KMeans;
 
 class SearchEngineResultSite extends SiteBase
 {
@@ -19,11 +21,36 @@ class SearchEngineResultSite extends SiteBase
         $dom->loadFromUrl($url);
         $links = $dom->find('#' . $this->config->result_id . ' a');
 
-        $content = '';
-        foreach ($links as $link) {
-            $content .= '<p>' . $link . '</p>';
+        $svgs = $dom->find('svg');
+        foreach ($svgs as $svg) {
+            $svg->setAttribute('viewBox', '');
         }
 
-        return $this->getHtml($content);
+        $content = '';
+        $lens = [];
+        foreach ($links as $link) {
+            $data = [
+                strlen($link),
+                strlen($link->text()),
+                count($link->getChildren()),
+                count($link->getAttributes())
+            ];
+            $lens[(string) $link] = $data;
+            //echo json_encode($data). $link->text().PHP_EOL;
+            //if ($len < 100) {
+                //echo $link->innerHtml() . PHP_EOL;
+            //}
+            $content .= '<p>' . $link . '</p>';
+        }
+        $kmeans = new KMeans(4);
+        $res = $kmeans->cluster($lens);
+        usort(
+            $res,
+            function ($a, $b) {
+                return count($a) - count($b);
+            }
+        );
+
+        return $this->getHtml(implode('<hr>', array_keys($res[2])));
     }
 }
