15afcd0f5009035ade2c06879f39d11a587a3781
Olle Harstedt <olleharstedt@yahoo.com>
2019-09-19 19:23:05 +0200

    Minimal graph test

--- src/spikes/link_cluster/link_cluster.php
+++ src/spikes/link_cluster/link_cluster.php
@@ -8,5 +8,23 @@ namespace zenmodedaemon;
 
 require "vendor/autoload.php";
 
-$plot = new PHPlot(800, 600);
+$plot = new \PHPlot(800, 600);
 $plot->SetImageBorderType('plain');
+
+$data = [
+    [0, 10],
+    [1, 20],
+    [1, 20]
+];
+
+$plot->SetPlotType('bars');
+$plot->SetDataType('text-data');
+$plot->SetDataValues($data);
+
+// Main plot title:
+$plot->SetTitle('Links');
+
+// Make sure Y axis starts at 0:
+$plot->SetPlotAreaWorld(null, 0, null, null);
+
+$plot->DrawGraph();
